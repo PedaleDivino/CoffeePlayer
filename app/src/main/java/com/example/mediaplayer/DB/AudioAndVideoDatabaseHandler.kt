460fb2ed4c26957f020564fad92d196020329e8f
William-Hourigan <william.a.hourigan@gmail.com>
2022-12-12 14:38:40 +0100

    Commenti sul database (William H.)

--- app/src/main/java/com/example/mediaplayer/DB/AudioAndVideoDatabaseHandler.kt
+++ app/src/main/java/com/example/mediaplayer/DB/AudioAndVideoDatabaseHandler.kt
@@ -23,77 +23,89 @@ class AudioAndVideoDatabaseHandler(context: Context): SQLiteOpenHelper(context, 
 
     //  FUNZIONI CHE LAVORANO SULLA TABELLA DEI FILE
 
+
     /*
     CREATE
      */
 
+
     //  Funzione che mi aggiunge un nuovo elemento alla tabella dei file
+    //  (l'elemento da aggiungere alla tabella è passata come)
     fun createFile(file: AudioAndVideo) {
-        var db: SQLiteDatabase = writableDatabase
+        var db: SQLiteDatabase = writableDatabase           //  Variabile che può accedere al database
 
-        var values: ContentValues = ContentValues()
-        values.put(KEY_FILE_NAME, file.fileName)
-        values.put(KEY_FILE_TYPE, file.fileType)
-        values.put(KEY_FILE_PATH, file.filePath)
+        var values: ContentValues = ContentValues()         //  Variabile che conterrà tutti i dati che saranno inseriti nella tabella
+        values.put(KEY_FILE_NAME, file.fileName)            //  Dico alla variabile "values" che deve salvare nella colonna "KEY_FILE_NAME" il nome del file passato come parametro
+        values.put(KEY_FILE_TYPE, file.fileType)            //  Dico alla variabile "values" che deve salvare nella colonna "KEY_FILE_TYPE" il tipo del file passato come parametro
+        values.put(KEY_FILE_PATH, file.filePath)            //  Dico alla variabile "values" che deve salvare nella colonna "KEY_FILE_PATH" la posizione in memoria del file passato come parametro
 
-        db.insert(TABLE_FILE, null, values)
+        db.insert(TABLE_FILE, null, values)         //  Inserisco nel database i valori che sono stati assegnati a "values"
 
-        //Log.d("DATA INSERITI", "SUCCESS")
-        db.close()
+        db.close()          //  Chiudo la connesione al database
     }
-
 
 
     /*
     READ
      */
 
+
     //  Funzione che ritorna un file che ha come ID l'intero passato come parametro
     @SuppressLint("Range")
     fun readAtodo(id: Int): AudioAndVideo{
-        var db: SQLiteDatabase = readableDatabase
-        var cursor: Cursor = db.query(TABLE_FILE, null, KEY_FILE_ID+"=?", arrayOf(id.toString()), null, null, null, null)
+        var db: SQLiteDatabase = readableDatabase           //  Variabile "db" di tipo SQLiteDatabase che può accedere al database
 
-        cursor.moveToFirst()
+        var cursor: Cursor = db.query(          //  Variabile "cursor" di tipo Cursor che fornisce un interfaccia al risultato della query che poniamo al database e richiamo di una funzione query sul database
+            TABLE_FILE,                         //  Indico a quale tabella stiamo facendo la query
+            null,                       //  Indico che colonne voglio nel ritorno dell'operazione (ponendo null indico che voglio tutte le colonne)
+            KEY_FILE_ID+"=?",           //  Indico che la condizione sull'ID del file sarà passato nel prossimo parametro
+            arrayOf(id.toString()),             //  Indico l'ID del file che voglio trovare (il valore passato per parametro)
+            null,                       //  Indico di non fare il groupBy
+            null,                        //  Indico di non fare il having
+            null,                       //  Indico di non fare l'orderBy
+            null)                         //  Indico di non mettere un limite al numero di righe da restituire e chiudo il richiamo della funzione query
 
-        var file = AudioAndVideo()
-        file.fileName = cursor.getString(cursor.getColumnIndex(KEY_FILE_NAME))
-        file.fileType = cursor.getString(cursor.getColumnIndex(KEY_FILE_TYPE))
-        file.filePath = cursor.getString(cursor.getColumnIndex(KEY_FILE_PATH))
+        cursor.moveToFirst()                   //  Punto il cursore alla prima riga del risultato che gli ha dato la query (in questo caso è l'unica riga)
 
-        return file
+        var file = AudioAndVideo()             //  Variabile "file" di tipo AudioAndVideo
+        //NON SO ANCORA SE LA VOGLIAMO  file.id = cursor.getInt(cursor.getColumnIndex(KEY_FILE_ID))             //  Passo alla variabile file l'id del file desiderato
+        file.fileName = cursor.getString(cursor.getColumnIndex(KEY_FILE_NAME))          //  Passo alla variabile file il nome del file desiderato (preso dalla tabella dei file)
+        file.fileType = cursor.getString(cursor.getColumnIndex(KEY_FILE_TYPE))          //  Passo alla variabile file il tipo del file desiderato (preso dalla tabella dei file)
+        file.filePath = cursor.getString(cursor.getColumnIndex(KEY_FILE_PATH))          //  Passo alla variabile file il path del file desiderato (preso dalla tabella dei file)
+
+        return file         //  Ritorno la variabile file riempita dai dati che abbiamo preso dal database tramite la query
     }
 
 
     //  Funzione che ritorna un array di file contenente tutti i file che abbiamo aggiunto alla tabella
     @SuppressLint("Range")
     fun readToDo(): ArrayList<AudioAndVideo> {
-        var db: SQLiteDatabase = readableDatabase
-        var list: ArrayList<AudioAndVideo> = ArrayList()
-        var selectAll = "SELECT * FROM $TABLE_FILE"
-        var cursor: Cursor = db.rawQuery(selectAll, null)
+        var db: SQLiteDatabase = readableDatabase                              //  Variabile "db" di tipo SQLiteDatabase che può accedere al database
+        var list: ArrayList<AudioAndVideo> = ArrayList()                       //  Variabile "list" che è un ArrayList di tipo AudioAndVideo che conterrà tutti i dati dei file salvati nel database
+        var selectAll = "SELECT * FROM $TABLE_FILE"                            //  Variabile "selectAll" di tipo stringa che salva la query da porre al database (che in questo caso richiede tutta la tabella TABLE_FILE)
+        var cursor: Cursor = db.rawQuery(selectAll, null)           //  Variabile "cursor" di tipo Cursor che fornisce un interfaccia al risultato della query che poniamo al database e richiamo di una funzione rawQuery sul database
+        var file=AudioAndVideo()                                               //  Variabile "file" di tipo AudioAndVideo che serve per compilare i file prima di metterli nella lista
 
-        if (cursor.moveToFirst()) {
-            do {
-                var file=AudioAndVideo()
-                file.id=cursor.getInt(cursor.getColumnIndex(KEY_FILE_ID))
-                file.fileName = cursor.getString(cursor.getColumnIndex(KEY_FILE_NAME))
-                file.fileType = cursor.getString(cursor.getColumnIndex(KEY_FILE_TYPE))
-                file.filePath = cursor.getString(cursor.getColumnIndex(KEY_FILE_PATH))
+        if (cursor.moveToFirst()) {                                            //  Se il risultato della query non è vuota punta il cursore alla prima riga del risultato
+            do {                                                                    //  Inizio di un ciclo do while
+                file.id=cursor.getInt(cursor.getColumnIndex(KEY_FILE_ID))                       //  Passo alla variabile file l'id del file corrente
+                file.fileName = cursor.getString(cursor.getColumnIndex(KEY_FILE_NAME))          //  Passo alla variabile file il nome del file corrente
+                file.fileType = cursor.getString(cursor.getColumnIndex(KEY_FILE_TYPE))          //  Passo alla variabile file il tipe del file corrente
+                file.filePath = cursor.getString(cursor.getColumnIndex(KEY_FILE_PATH))          //  Passo alla variabile file il path del file corrente
 
-                list.add(file)
-            } while (cursor.moveToNext())
+                list.add(file)                                                                  //  Aggiungo il file appena compilato alla lista
+            } while (cursor.moveToNext())                                           //  Ripeto il ciclo se il cursore non ha finito di leggere i risultato della query e passa alla prossima riga
         }
-        return list
+        return list                                                            //  Ritorno l'ArrayList "list" che contiene tutti i file
     }
 
 
     //  Funzione che controlla se la tabella file ha almeno un elemento
     fun pieno(): Boolean {
-        var db:SQLiteDatabase=readableDatabase
-        var istruzione = "SELECT * FROM $TABLE_FILE"
-        var cursorVuoto: Cursor = db.rawQuery(istruzione, null)
-        return cursorVuoto.moveToFirst()
+        var db:SQLiteDatabase = readableDatabase                                   //  Variabile "db" di tipo SQLiteDatabase che può accedere al database
+        var istruzione = "SELECT * FROM $TABLE_FILE"                               //  Variabile "istruzione" di tipo stringa che salva la query da porre al database (che in questo caso richiede tutta la tabella TABLE_FILE)
+        var cursorVuoto: Cursor = db.rawQuery(istruzione, null)         //  Variabile "cursor" di tipo Cursor che fornisce un interfaccia al risultato della query che poniamo al database e richiamo di una funzione rawQuery sul database
+        return cursorVuoto.moveToFirst()                                           //  Ritorno false se la query mi da un risultato vuoto, mentre ritorno vero se la query mi da un risultato non vuoto
     }
 
 
@@ -103,12 +115,12 @@ class AudioAndVideoDatabaseHandler(context: Context): SQLiteOpenHelper(context, 
 
 
     //  Funzione che rimpiazza un file del database con un altro che viene passato come parametro
-    fun replaceFile(file: AudioAndVideo): Int {
-        var db: SQLiteDatabase = writableDatabase
-        var values: ContentValues = ContentValues()
-        values.put(KEY_FILE_NAME, file.fileName)
-        values.put(KEY_FILE_TYPE, file.fileType)
-        values.put(KEY_FILE_PATH, file.filePath)
+    fun replaceFile(file: AudioAndVideo) {
+        var db: SQLiteDatabase = writableDatabase           //  Variabile "db" di tipo SQLiteDatabase che può accedere al database
+        var values: ContentValues = ContentValues()         //  Variabile "values" di tipo ContentValues che conterrà i valori che sovrascriveranno i valori attualmente nel database
+        values.put(KEY_FILE_NAME, file.fileName)            //  Pongo a values il nuovo valore della colonna KEY_FILE_NAME (cioè il nome del file che è stato passato come parametro)
+        values.put(KEY_FILE_TYPE, file.fileType)            //  Pongo a values il nuovo valore della colonna KEY_FILE_TYPE (cioè il tipo del file che è stato passato come parametro)
+        values.put(KEY_FILE_PATH, file.filePath)            //  Pongo a values il nuovo valore della colonna KEY_FILE_PATH (cioè il path del file che è stato passato come parametro)
 
         /*
         UPDATE = AGGIORNA UNA RIGA DEL DATABASE (HA BISOGNO DI 4 ARGOMENTI)
@@ -117,7 +129,11 @@ class AudioAndVideoDatabaseHandler(context: Context): SQLiteOpenHelper(context, 
         TERZO ARGOMENTO = LA RIGA DA AGGIORNARE
         QUARTO ARGOMENTO = GLI ELEMENTI CHE VERRANNO SOSTITUITI DA I NUOVI VALORI
          */
-        return db.update(TABLE_FILE, values, KEY_FILE_ID+"=?", arrayOf(file.id.toString()))
+        db.update(                                          //  Richiamo una funzione per aggiornare il database
+            TABLE_FILE,                                         //  Indico a quale tabella stiamo facendo la query
+            values,                                             //  Passo i nuovi valori che devono sovrascrivere i valori vecchi
+            KEY_FILE_ID+"=?",                        //  Passo
+            arrayOf(file.id.toString()))
     }
 
 

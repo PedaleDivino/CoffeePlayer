890527f96211457a2689ca85dc2334767934197f
Mark-Giu20 <marco20giuliani@gmail.com>
2022-12-18 00:06:54 +0100

    Aggiunti commenti a SassariMusic e add_tracks e selezione automatica del nome del file e aggiunta estensione del file alla fine del nome (Marco G.)

--- app/src/main/java/com/example/mediaplayer/SassariMusic.kt
+++ app/src/main/java/com/example/mediaplayer/SassariMusic.kt
@@ -13,16 +13,17 @@ import com.example.mediaplayer.DB.AudioAndVideoDatabaseHandler
 
 
 class SassariMusic : AppCompatActivity() {
-    val CHOOSE_FROM_DEVICE = 1001
-    var dbHandler : AudioAndVideoDatabaseHandler?= null
-    var trackList: ArrayList<AudioAndVideo> = ArrayList()
+    val CHOOSE_FROM_DEVICE = 1001   //Variabile che definisce il tipo di richiesta
+    var dbHandler : AudioAndVideoDatabaseHandler?= null //Variabile per la gestione del database
+    var trackList: ArrayList<AudioAndVideo> = ArrayList()   //ArrayList che conterrà tutti gli elementi all'interno del database
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        callChooseFileFromDevice()
+        callChooseFileFromDevice()  //Al momento dell'avvio dell'activity
     }
 
+    //Codice preso e riadattato da stackoverflow, link: https://stackoverflow.com/questions/20067508/get-real-path-from-uri-android-kitkat-new-storage-access-framework
     @SuppressLint("Range")
     fun getAudioPath(uri: Uri?): String? {
         var cursor = contentResolver.query(uri!!, null, null, null, null)
@@ -40,6 +41,7 @@ class SassariMusic : AppCompatActivity() {
         return path
     }
 
+    //Codice preso e riadattato da stackoverflow, link: https://stackoverflow.com/questions/20067508/get-real-path-from-uri-android-kitkat-new-storage-access-framework
     @SuppressLint("Range")
     fun getVideoPath(uri: Uri?): String? {
         var cursor = contentResolver.query(uri!!, null, null, null, null)
@@ -58,50 +60,48 @@ class SassariMusic : AppCompatActivity() {
     }
 
     private fun callChooseFileFromDevice(){
-        val intent : Intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        val mimetypes = arrayOf("audio/mpeg", "video/mp4")
+        val intent : Intent = Intent(Intent.ACTION_OPEN_DOCUMENT)   //Crea un intent per l'apertura del file system del telefono
+        val mimetypes = arrayOf("audio/mpeg", "video/mp4")  //Array che contiene i tipi di file che verranno visualizzati all'interno del file explorer e che saranno di conseguenza selezionabili(In questo caso MP3 e MP4)
         intent.setType("*/*")
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
-        startActivityForResult(intent, CHOOSE_FROM_DEVICE)
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes) //Inserisce la tipologia di file scelti all'interno dell'intent creato precedentemente
+        startActivityForResult(intent, CHOOSE_FROM_DEVICE)  //Avvia l'activity per la ricezione di informazioni, in questo caso l'uri del file selezionato dall'utente
     }
 
+    //Funzione per ottenere dati dall'activity del file explorer
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CHOOSE_FROM_DEVICE && resultCode == RESULT_OK) {
+        if (requestCode == CHOOSE_FROM_DEVICE && resultCode == RESULT_OK) { //Controlla se il codice di richiesta è quello giusto e se l'activity è terminata con successo
             if (data != null) {
-                val uri: Uri? = data.data
+                val uri: Uri? = data.data //Prende la uri del file selezionato
                 val cR: ContentResolver = this.getContentResolver()
-                val type = cR.getType(uri!!)
+                val type = cR.getType(uri!!)    //Ritorna il tipo del file selezionato
                 var path : String? = ""
                 if (type == "audio/mpeg"){
-                    path = getAudioPath(uri)
-                    Toast.makeText(this, "File audio selezionato", Toast.LENGTH_LONG).show()
+                    path = getAudioPath(uri)    //Esegue la conversione della uri un una path
                 } else {
-                    path = getVideoPath(uri)
-                    Toast.makeText(this, "File video selezionato", Toast.LENGTH_LONG).show()
+                    path = getVideoPath(uri)    //Esegue la conversione della uri un una path
                 }
-                dbHandler = AudioAndVideoDatabaseHandler(this)
-                trackList = dbHandler!!.readTracks()
+                dbHandler = AudioAndVideoDatabaseHandler(this)  //Viene inizializzato un oggetto di tipo AudioAndVideoDatabaseHandler per la gestione del database
+                trackList = dbHandler!!.readTracks()    //Tramite il metodo readTracks vengono presi tutti i dati dal database e convertiti in un oggetto di tipo ArrayList
                 var controllo = true
                 for (i in 0 until trackList.size) {
-                    if (trackList[i].filePath == path) {
+                    if (trackList[i].filePath == path) {    //Controllo se la path non è già stata inserita all'interno del database
                         controllo = false
                         break
                     }
                 }
-                if (controllo) {
+                if (controllo) {    //Se non è stata inserita andrà ad avviare l'activity add_tracks per l'inserimento del nome del file e l'aggiunta al database
                     val addTracksInt : Intent = Intent(this, add_tracks::class.java)
-                    addTracksInt.putExtra("Path", path)
-                    addTracksInt.putExtra("Type", type)
-                    startActivity(addTracksInt)
+                    addTracksInt.putExtra("Path", path) //Passa la path alla activity add_tracks
+                    addTracksInt.putExtra("Type", type) //Passa il tipo del file alla activity add_tracks
+                    startActivity(addTracksInt) //Avvia l'activity add_traks
                 }
-                else {
-                    Toast.makeText(this, "FILE GIA SELEZIONATO", Toast.LENGTH_LONG).show()
+                else {  //Se la path è già stata selezionata avvertirà l'utente e riavvierà l'activity per permettere all'utente di selezionare un nuovo file
+                    Toast.makeText(this, "FILE GIA SELEZIONATO", Toast.LENGTH_SHORT).show()
                     startActivity(Intent(this, SassariMusic::class.java))
                 }
             }
-        } else {
+        } else {    //Se il file explorer viene chiuso prima della selezione di un file verrà riavviata l'activity main
             Toast.makeText(this, "Non è stato selezionato nessun file", Toast.LENGTH_LONG).show()
             startActivity(Intent(this, MainActivity::class.java))
         }
--- app/src/main/java/com/example/mediaplayer/add_tracks.kt
+++ app/src/main/java/com/example/mediaplayer/add_tracks.kt
@@ -8,38 +8,50 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mediaplayer.DB.AudioAndVideo
 import com.example.mediaplayer.DB.AudioAndVideoDatabaseHandler
+import java.io.File
 
 class add_tracks : AppCompatActivity() {
     var dbHandler : AudioAndVideoDatabaseHandler ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_tracks)
-        val extras = intent.extras
+        val extras =
+            intent.extras  //Inizializza una variabile per recuperare i valori passati dall'activity precedente
         dbHandler = AudioAndVideoDatabaseHandler(this)
-        val fileNameEdit : EditText = findViewById(R.id.FileName)
-        val addTracks : Button = findViewById(R.id.AddTrack)
-        var extraUri : String= ""
-        var typeFile : String = ""
-        if (extras != null) {
+        val fileNameEdit: EditText =
+            findViewById(R.id.FileName)   //Associa l'EditText FileName ad una variabile
+        val addTracks: Button =
+            findViewById(R.id.AddTrack)    //Associa il bottone AddTrack ad una variabile
+        var extraUri: String = ""
+        var typeFile: String = ""
+        if (extras != null) {   //Controlla se ci sono dei valori passati dall'activity precedente
+            //Tramite una key identificativa assegnata ad ogni valore per essere trasmesso i valori vengono assegnati a delle variabili locali
             extraUri = extras.getString("Path").toString()
             typeFile = extras.getString("Type").toString()
         }
-        addTracks.setOnClickListener(){
-            if (fileNameEdit.text.isBlank()){
-                fileNameEdit.setError("Non può essere lasciato vuoto")
+        addTracks.setOnClickListener() {
+            var fileName: String = ""
+            if (fileNameEdit.text.isBlank()) {   //Controllo se l'EditText è stato lasciato vuoto
+                fileName = File(extraUri).name  //Se l'EditText è stato lasciato vuoto prende il nome del file originale
             } else {
-                val fileName : String = fileNameEdit.text.toString()
-                if (typeFile == "audio/mpeg"){
-                    val file = AudioAndVideo(null, fileName, "mp3", extraUri)
-                    dbHandler!!.createFile(file)
-                    Toast.makeText(this, "File audio aggiunto", Toast.LENGTH_LONG).show()
-                } else {
-                    val file = AudioAndVideo(null, fileName, "mp4", extraUri)
-                    dbHandler!!.createFile(file)
-                    Toast.makeText(this, "File video aggiunto", Toast.LENGTH_LONG).show()
-                }
-                startActivity(Intent(this, MainActivity::class.java))
+                fileName =
+                    fileNameEdit.text.toString() //Prende la stringa inserita all'interno dell'editText dall'utente
             }
+            if (typeFile == "audio/mpeg") {
+                if (!fileName.contains(".mp3")) {    //Controlla se fileName non contiene l'estensione del file .mp3
+                    fileName += ".mp3"  //Aggiunge al nome del file .mp3
+                }
+                val file = AudioAndVideo(null, fileName, "mp3", extraUri)
+                dbHandler!!.createFile(file)    //Richiama il metodo createFile per l'aggiunta del nuovo file al database
+                Toast.makeText(this, "File audio aggiunto", Toast.LENGTH_LONG).show()
+            } else {
+                if (!fileName.contains(".mp4")) //Controlla se fileName non contiene l'estensione del file .mp4
+                    fileName += ".mp4"  //Aggiunge al nome del file .mp3
+            }
+            val file = AudioAndVideo(null, fileName, "mp4", extraUri)
+            dbHandler!!.createFile(file)    //Richiama il metodo createFile per l'aggiunta del nuovo file al database
+            Toast.makeText(this, "File video aggiunto", Toast.LENGTH_LONG).show()
         }
+        startActivity(Intent(this, MainActivity::class.java))   //Avvia la MainActivity
     }
 }
